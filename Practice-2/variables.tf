2c8ca4c372d7d217d2eace3518245cd313fbe3a5
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-11 11:07:52 +0530

    Update variables.tf

--- Practice-2/variables.tf
+++ Practice-2/variables.tf
@@ -1,5 +1,24 @@
-variable "instance_type" {
-  description = "The instance type for the EC2 instance"
+variable "aws_region" {
   type        = string
-  default     = "t2.micro"
+  description = "The AWS region to use for resources"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "The instance type for the EC2 instance"
+}
+
+variable "ami_id" {
+  type        = string
+  description = "The ID of the AMI to use for the EC2 instance"
+}
+
+variable "aws_access_key" {
+  type        = string
+  description = "Using a access key to deploy aws resources"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "Using a secret key to deploy aws resources"
 }
