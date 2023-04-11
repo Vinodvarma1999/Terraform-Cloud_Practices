408d27fbaee4e06e54d4e2c7316f7d18f62a4fe5
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-11 15:01:45 +0530

    Update variables.tf

--- Practice-4/variables.tf
+++ Practice-4/variables.tf
@@ -22,3 +22,39 @@ variable "aws_secret_key" {
   type        = string
   description = "Using a secret key to deploy aws resources"
 }
+
+variable "db_username" {
+  description = "The username for the database"
+  type        = string
+  #default     = "admin"
+}
+
+variable "db_password" {
+  description = "The password for the database"
+  type        = string
+  default     = "admin9988"
+}
+
+variable "db_instance_class" {
+  description = "The instance class for the RDS instance"
+  type        = string
+  default     = "db.t2.micro"
+}
+
+variable "db_engine" {
+  description = "The database engine for the RDS instance"
+  type        = string
+  default     = "mysql"
+}
+
+variable "db_engine_version" {
+  description = "The version of the database engine for the RDS instance"
+  type        = string
+  default     = "5.7"
+}
+
+variable "db_storage_size" {
+  description = "The storage size for the RDS instance"
+  type        = number
+  default     = 20
+}
