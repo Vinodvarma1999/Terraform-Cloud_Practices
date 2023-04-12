a9a6232ced4d558a1a1b6782b376a8571dc889ae
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-12 14:42:46 +0530

    Update main.tf

--- Practice-6/main.tf
+++ Practice-6/main.tf
@@ -1,16 +1,13 @@
+# To create a workspace in organization
 resource "tfe_workspace" "test" {
   name         = "my-workspace-name"
   tag_names    = ["test", "app"]
-  vcs_repo {
-    oauth_token_id = var.oauthgithubtoken
-    identifier = "https://github.com/Vinodvarma1999/Terraform-Cloud_Practices"
-    branch     = "main"
-  }
   working_directory = "./Practice-6"
   auto_apply        = true
   queue_all_runs    = true
 }
 
+# To create a variable in workspace
 resource "tfe_variable" "my-var" {
   key          = "username"
   value        = var.db_username
