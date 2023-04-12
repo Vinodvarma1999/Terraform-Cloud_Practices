0f883bb9a316d13288728e5167d6aae456785cfa
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-12 15:05:18 +0530

    Update main.tf

--- Practice-7/main.tf
+++ Practice-7/main.tf
@@ -1,20 +1,20 @@
-resource "tfe_organization" "test" {
+resource "tfe_organization" "UAT-Test" {
   name  = "my-org-name"
   email = "xyz@gmail.com"
 }
 
-resource "tfe_workspace" "test" {
+resource "tfe_workspace" "UAT-Test" {
   name         = "my-workspace-name"
-  organization = tfe_organization.test.name
+  organization = tfe_organization.UAT-Test.name
 }
 
-resource "tfe_variable_set" "test" {
+resource "tfe_variable_set" "UAT-Test" {
   name          = "Test-variable-test"
   description   = "Some-description."
-  organization  = tfe_organization.test.name
+  organization  = tfe_organization.UAT-Test.name
 }
 
 resource "tfe_workspace_variable_set" "test" {
-  variable_set_id = tfe_variable_set.test.id
-  workspace_id    = tfe_workspace.test.id
+  variable_set_id = tfe_variable_set.UAT-Test.id
+  workspace_id    = tfe_workspace.UAT-Test.id
 }
