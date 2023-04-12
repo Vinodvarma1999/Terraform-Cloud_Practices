62d8a006ec5a4b547aea5e1e27814173a6a83921
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-12 11:51:27 +0530

    Update main.tf

--- Practice-6/main.tf
+++ Practice-6/main.tf
@@ -7,15 +7,13 @@ resource "tfe_workspace" "test" {
   }
   working_directory = "./Practice-6"
   auto_apply        = true
-  vcs_trigger {
-    identifier = "https://github.com/Vinodvarma1999/Terraform-Cloud_Practices.git"
-    branch     = "main"
-  }
+  queue_all_runs    = true
 }
 
-resource "tfe_workspace_variable" "my-var" {
+resource "tfe_variable" "my-var" {
   key          = "username"
   value        = var.db_username
   sensitive    = true
   workspace_id = tfe_workspace.test.id
+  description  = "a useful description"
 }
