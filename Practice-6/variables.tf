26a77d62b71706af0a1f22016a1635a8d291a74f
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-12 11:42:04 +0530

    Update variables.tf

--- Practice-6/variables.tf
+++ Practice-6/variables.tf
@@ -2,3 +2,8 @@ variable "token" {
  type         = string
  description  = "This token is used to deploy resource on terraform cloud"
 }
+
+variable "db_username" {
+  type        = string
+  description = "A sensitive variable"
+}
