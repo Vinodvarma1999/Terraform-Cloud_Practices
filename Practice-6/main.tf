f8fa3a25fca03970794fca8135cb20392e979c72
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-12 12:14:19 +0530

    Update main.tf

--- Practice-6/main.tf
+++ Practice-6/main.tf
@@ -2,10 +2,7 @@ resource "tfe_workspace" "test" {
   name         = "my-workspace-name"
   tag_names    = ["test", "app"]
   vcs_repo {
-    oauth_token {
-      id = var.oauthgithubtoken
-    }
-#     oauth_token_id = var.oauthgithubtoken
+    oauth_token_id = var.oauthgithubtoken
     identifier = "https://github.com/Vinodvarma1999/Terraform-Cloud_Practices.git"
     branch     = "main"
   }
