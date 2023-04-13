2b2c6be7015f1d330d4187e2fc45cbc83b5d09d5
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-13 15:56:31 +0530

    Update main.tf

--- Practice-9/main.tf
+++ Practice-9/main.tf
@@ -1,9 +1,11 @@
-resource "tfe_policy" "test" {
-  name         = "my-policy-name"
-  description  = "This policy always passes"
-  organization = "vinod0510"
-  kind         = "opa"
-  policy       = "package example rule[\"not allowed\"] { false }"
-  query        = "data.example.rule"
-  enforce_mode = "mandatory"
+resource "github_repository" "example" {
+  name        = "practice"
+  description = "My awesome codebase"
+  visibility = "public"
+
+  template {
+    owner                = "github"
+    repository           = "terraform-template-module"
+    include_all_branches = true
+  }
 }
