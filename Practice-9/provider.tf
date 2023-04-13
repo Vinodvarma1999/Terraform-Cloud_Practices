38437a18b12b710ce9da4fa29f5967a908201be6
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-13 15:54:50 +0530

    Update provider.tf

--- Practice-9/provider.tf
+++ Practice-9/provider.tf
@@ -1,4 +1,12 @@
-provider "tfe" {
-  organization = "vinod0510"
-  token        = var.token
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.token 
 }
