024c02fbd76ed6d09eaeff0d53df94e5064ccfcf
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-13 16:18:46 +0530

    Update provider.tf

--- Practice-9/provider.tf
+++ Practice-9/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     github = {
-      source = "integrations/terraform-provider-github/github"
+      source = "integrations/github"
       version = "5.22.0"
     }
   }
