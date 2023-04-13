a898733672eeadfb4e8ca41888808d59cdb93c17
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-13 15:21:16 +0530

    Update main.tf

--- Practice-8/main.tf
+++ Practice-8/main.tf
@@ -5,3 +5,45 @@ resource "tfe_team" "my-team" {
     manage_vcs_settings = true
   }
 }
+
+# Associate a team to permissions on a workspace
+
+resource "tfe_team_access" "test" {
+  access       = "read"             # It can be admin, read, plan, or write
+  team_id      = tfe_team.my-team.id
+  workspace_id = # Define the workspace 
+}
+
+# Add or remove a user from a team
+
+resource "tfe_team_member" "test" {
+  team_id  = tfe_team.test.id
+  username = "sander"
+}
+
+# # Add Multiple User 
+
+# locals {
+#   all_usernames = toset([
+#     "user1",
+#     "user2",
+#   ])
+# }
+
+# resource "tfe_team_members" "test" {
+#   team_id   = tfe_team.my-team.id
+#   usernames = [for user in local.all_usernames : user]  
+# }
+
+# Generates a new team token and overrides existing token if one exists.
+
+resource "tfe_team_token" "test" {
+  team_id = tfe_team.my-team.id
+}
+
+
+  
+
+  
+  
+
