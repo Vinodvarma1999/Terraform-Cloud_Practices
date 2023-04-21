f5d95bbf0b8b46e7a3687ccea83a9ce310eef31b
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-21 12:16:44 +0530

    Create variables.tf

---
+++ Practice-10/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type        = string
+  description = "The AWS region to use for resources"
+}
+
+variable "aws_access_key" {
+  type        = string
+  description = "Using a access key to deploy aws resources"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "Using a secret key to deploy aws resources"
+}
