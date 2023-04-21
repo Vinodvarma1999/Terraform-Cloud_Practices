4e3e653ff18325b701b8bb2620734a94747ad004
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-21 13:09:36 +0530

    Create main.tf

---
+++ Practice-10/main.tf
@@ -0,0 +1,245 @@
+# Creating a VPC
+resource "aws_vpc" "my-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+# Creating a Security Group
+resource "aws_security_group" "My-SG" {
+  name = "My-SG"
+  description = "Allow SSH and HTTP Connection"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = aws_vpc.my-vpc.id
+  tags = {
+    Name = "My-SG"
+  }
+}
+
+# Creating a Subnet-1
+resource "aws_subnet" "my-subnet-1" {
+  vpc_id = aws_vpc.my-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet-1"
+  }  
+}
+
+# Creating a Subnet-2
+resource "aws_subnet" "my-subnet-2" {
+  vpc_id = aws_vpc.my-vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet-2"
+  }  
+}
+
+# Creating a IGW
+resource "aws_internet_gateway" "my-igw" {
+  vpc_id = aws_vpc.my-vpc.id
+  depends_on = [aws_vpc.my-vpc]
+  tags = {
+    Name = "IGW-project-1"
+  } 
+}
+
+# Creating a public Route table
+resource "aws_route_table" "my-pubRT" {
+  vpc_id = aws_vpc.my-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-igw.id
+  }
+  tags = {
+    Name = "my-public-RT-"
+  } 
+}
+
+# attaching a subnet to public Route table
+resource "aws_route_table_association" "my-pubRT-assoc" {
+  subnet_id = [aws_subnet.my-subnet-1.id, aws_subnet.my-subnet-2.id]
+  route_table_id = aws_route_table.my-pubRT.id  
+}
+
+# EKS Cluster IAM Role
+resource "aws_iam_role" "cluster" {
+  name = "${var.project}-Cluster-Role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+# EKS Cluster
+resource "aws_eks_cluster" "my-eks" {
+  name     = "my-eks"
+  role_arn = aws_iam_role.cluster.arn
+  version  = "1.21"
+
+  vpc_config {
+    # security_group_ids      = [aws_security_group.eks_cluster.id, aws_security_group.eks_nodes.id]
+    subnet_ids              = flatten([my-subnet-1.id, my-subnet-1.id])
+    endpoint_private_access = true
+    endpoint_public_access  = true
+    public_access_cidrs     = ["0.0.0.0/0"]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy
+  ]
+}
+
+# EKS Cluster Security Group
+resource "aws_security_group" "eks_cluster" {
+  name        = "my-cluster-sg"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = aws_vpc.my-vpc.id
+  tags = {
+    Name = "my-cluster-sg"
+  }
+}
+
+# EKS Node Security Group
+resource "aws_security_group" "eks_nodes" {
+  name        = "my-node-sg"
+  description = "Security group for all nodes in the cluster"
+  vpc_id      = aws_vpc.this.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name  = "my-node-sg"
+  }
+}
+
+resource "aws_security_group_rule" "nodes_internal" {
+  description              = "Allow nodes to communicate with each other"
+  from_port                = 0
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.eks_nodes.id
+  source_security_group_id = aws_security_group.eks_nodes.id
+  to_port                  = 65535
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "nodes_cluster_inbound" {
+  description              = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
+  from_port                = 1025
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_nodes.id
+  source_security_group_id = aws_security_group.eks_cluster.id
+  to_port                  = 65535
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "cluster_inbound" {
+  description              = "Allow worker nodes to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster.id
+  source_security_group_id = aws_security_group.eks_nodes.id
+  to_port                  = 443
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "cluster_outbound" {
+  description              = "Allow cluster API Server to communicate with the worker nodes"
+  from_port                = 1024
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster.id
+  source_security_group_id = aws_security_group.eks_nodes.id
+  to_port                  = 65535
+  type                     = "egress"
+}
+
+# EKS Node IAM Role
+resource "aws_iam_role" "node" {
+  name = "my-Worker-Role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node.name
+}
+
+output "cluster_name" {
+  value = aws_eks_cluster.my-eks.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.my-eks.endpoint
+}
+
+output "cluster_ca_certificate" {
+  value = aws_eks_cluster.my-eks.certificate_authority[0].data
+}
