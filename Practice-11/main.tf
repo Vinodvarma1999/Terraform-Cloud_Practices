8f0e1cf369e1b56609434bdff14a32c093390ed8
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-25 11:24:23 +0530

    Update main.tf

--- Practice-11/main.tf
+++ Practice-11/main.tf
@@ -3,6 +3,6 @@ resource "github_repository" "example" {
   description        = "My awesome codebase"
   auto_init          = true   # to initialize the repository with a README file
   allow_merge_commit = false  # to disallow merge commits.
-  #private            = true   # to create a private repository
-  visibility         = "internal"  # means resource is visible to all members of the organization or workspace that the resource belongs to, but not to external users or anonymous users.
+  private            = true   # to create a private repository
+  #visibility         = "internal"  # means resource is visible to all members of the organization or workspace that the resource belongs to, but not to external users or anonymous users.
 }
