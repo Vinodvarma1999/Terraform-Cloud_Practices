52b6d3d8f05c7c35c934bc9b62f2bc71eb2f7abf
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-25 13:05:53 +0530

    Update main.tf

--- Practice-11/main.tf
+++ Practice-11/main.tf
@@ -4,4 +4,4 @@ resource "github_repository" "example" {
   auto_init          = true   # to initialize the repository with a README file
   allow_merge_commit = false  # to disallow merge commits.
   visibility         = "private"  # means resource is visible to all members of the organization or workspace that the resource belongs to, but not to external users or anonymous users.
-}
+  owner       = "Vinodvarma1999
