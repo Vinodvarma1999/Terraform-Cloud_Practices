cc013e559ffdc3f342482226cb400c305e612498
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-25 10:39:56 +0530

    Create main.tf

---
+++ Practice-11/main.tf
@@ -0,0 +1,8 @@
+resource "github_repository" "example" {
+  name               = "example"
+  description        = "My awesome codebase"
+  auto_init          = true   # to initialize the repository with a README file
+  allow_merge_commit = false  # to disallow merge commits.
+  private            = true   # to create a private repository
+  visibility         = "internal"  # means resource is visible to all members of the organization or workspace that the resource belongs to, but not to external users or anonymous users.
+}
