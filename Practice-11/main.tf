63a4e462874f7e4adcf17efc59b584357def8149
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-25 13:08:07 +0530

    Update main.tf

--- Practice-11/main.tf
+++ Practice-11/main.tf
@@ -3,6 +3,5 @@ resource "github_repository" "example" {
   description        = "My awesome codebase"
   auto_init          = true   # to initialize the repository with a README file
   allow_merge_commit = false  # to disallow merge commits.
-  visibility         = "private"  # means resource is visible to all members of the organization or workspace that the resource belongs to, but not to external users or anonymous users.
-  owner       = "Vinodvarma1999"
+  visibility         = "public"  # means resource is visible to all members of the organization or workspace that the resource belongs to, but not to external users or anonymous users.
   }
