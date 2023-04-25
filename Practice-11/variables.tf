b2204c951839a4c39a5babab9b73bd02c510df6c
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-04-25 11:43:49 +0530

    Update variables.tf

--- Practice-11/variables.tf
+++ Practice-11/variables.tf
@@ -1,4 +1,4 @@
-variable "hubtoken" {
+variable "github_token" {
   type  = string
   description = "This token is used to create a repo on gihub account"
   default = "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"
