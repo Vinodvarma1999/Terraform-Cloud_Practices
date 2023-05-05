7be832172eeadde86ccc972d6526773530412c4f
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-05-05 23:02:52 +0530

    Create main.tf

---
+++ Aws-Terraform/main.tf
@@ -0,0 +1,32 @@
+data "aws_vpc" "example" {
+  filter {
+    name   = "tag:Name"
+    values = ["my-vpc"]
+  }
+}
+
+data "aws_ami" "example" {
+  owners = ["self"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["my-AMI"]
+  }
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami = data.aws_ami.example.id
+  instance_type = var.instance_type
+  subnet_id = data.aws_subnet.example["subnet_1"].id           
+  vpc_security_group_ids = [
+    var.security_groups["sg_1"], 
+  ]
+  tags = {
+    Name = "my-EC2"
+  }
+}
