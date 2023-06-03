e33d65fe137bfdfc8255dd82a8fce5c24f9aeba6
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-03 16:28:49 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,5 +1,5 @@
 module "my-module" {
-  source          = "./Modules/ec2/"
+  source          = ".//Modules//ec2"
   instance_type   = "t3.micro"
   instance_count  = 2
   aws_region      = "us-east-1" 
