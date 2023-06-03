e31cee4158267be8198f1d16caf6888496c77691
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-03 16:53:16 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,3 +1,9 @@
 module "my-module" {
   source          = "..//Modules//ec2"
+  ami             = var.aws_ami
+  instance_type   = var.instance_type
+  count           = var.instance_count
+  subnet_id       = var.aws_subnet_id
+  vpc_security_group_ids = var.aws_security_group
+
 }
