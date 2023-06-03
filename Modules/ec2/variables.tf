cc096d8ac924356bca28ff69765171674b776372
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-03 16:01:31 +0530

    Update variables.tf

--- Modules/ec2/variables.tf
+++ Modules/ec2/variables.tf
@@ -1,17 +1,14 @@
 variable "instance_type" {
   type        = string
-  default     = "t2.micro"
   description = "The instance type for the EC2 instance"
 }
 
 variable "instance_count" {
   type = number
-#   default = 12
 }  
 
 variable "aws_region" {
   type        = string
-  default     = "us-east-1"
   description = "The AWS region to use for resources"
 }
 
@@ -26,43 +23,10 @@ variable "aws_secret_key" {
 }
 
 variable "subnets" {
-  type = map(object({
-    id                = string
-    availability_zone = string
-  }))
-  default = {
-    subnet_1 = {
-      id                = "subnet-0cdf91acd355832da"
-      availability_zone = "us-east-1a"
-    },
-    # subnet_2 = {
-    #   id                = "subnet-00c55c0db4e18f334"
-    #   availability_zone = "us-east-1b"
-    # },
-  }
+  type = string
 }
 
 variable "security_groups" {
-  type = map(string)
-  default = {
-    sg_1 = "sg-028dc593103896d09"
-    # sg_2 = "sg-0123456789abcdef1"
-  }
-}
-
-variable "location" {
-  default = "IBCI"
-}
-
-variable "app_name" {
-  default = "XPC"
-}
-
-variable "env" {
-  default = 3
-}
-
-variable "service_name" {
   type = string
-  default = "VM"
-} 
+}
+
