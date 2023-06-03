b2be892570ef7c8e6721da11a1412f3d5b6fb226
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-03 16:57:47 +0530

    Create variables.tf

---
+++ ec2/variables.tf
@@ -0,0 +1,8 @@
+variable "instance_type" {}
+variable "aws_ami" {}
+variable "instance_count" {}
+variable "aws_region" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_security_group" {}
+variable "aws_subnet_id" {}
