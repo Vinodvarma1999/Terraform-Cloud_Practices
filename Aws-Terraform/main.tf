a46151a5c0e98d4325ccd975c2e1f80b280757eb
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-03 12:31:27 +0530

    Update main.tf

--- Aws-Terraform/main.tf
+++ Aws-Terraform/main.tf
@@ -43,7 +43,7 @@ resource "aws_subnet" "my-subnet" {
 locals {
   last_used_number = try(file("${path.module}/last_used_number.txt"), "1")
   formatted_number = format("%03d", local.last_used_number)
-  truncated_number = substr(local.formatted_number, -2)
+  truncated_number = substr(local.formatted_number, -2, 2)
 }
 
 # Write the last used number to a local file
