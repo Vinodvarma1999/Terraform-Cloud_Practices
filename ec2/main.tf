466cd4afd67fbdabe5ec3eba9fa4af2083e6461b
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-03 17:05:12 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,7 +3,7 @@ module "my-module" {
   aws_ami             = var.aws_ami
   instance_type       = var.instance_type
   aws_subnet_id       = var.aws_subnet_id
-  aws_security_group  = var.aws_security_group
+  vpc_security_group_id  = var.aws_security_group
   instance_count      = var.instance_count
   aws_region          = var.aws_region
   aws_access_key      = var.aws_access_key
