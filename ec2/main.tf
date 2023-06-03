ed4bd8f43f67c1022f8ff1fd25d89010b336402e
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-03 16:30:15 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,7 +2,4 @@ module "my-module" {
   source          = "..//Modules//ec2"
   instance_type   = "t3.micro"
   instance_count  = 2
-  aws_region      = "us-east-1" 
-  access_key      = var.aws_access_key
-  secret_key      = var.aws_secret_key
 }
