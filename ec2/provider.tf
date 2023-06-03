6c24f29fddc4ddf42bdb2ac6f4a09a572181ee03
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-03 16:33:40 +0530

    Update provider.tf

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  aws_region      = "us-east-1" 
+  region      = "us-east-1" 
   access_key      = "AKIA5TO7FP6W3U6CG43V"
   secret_key      = "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"
 }
