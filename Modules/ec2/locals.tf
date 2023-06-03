500f4108490e6da1c39d1431cab8a80e1e6d9c17
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-03 16:01:52 +0530

    Create locals.tf

---
+++ Modules/ec2/locals.tf
@@ -0,0 +1,19 @@
+variable "location" {
+  type = string
+  default = "IBCI"
+}
+
+variable "app_name" {
+  type = string
+  default = "XPC"
+}
+
+variable "env" {
+  type = string
+  default = 3
+}
+
+variable "service_name" {
+  type = string
+  default = "VM"
+} 
