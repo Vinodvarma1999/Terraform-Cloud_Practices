7b582409da6daed3bb6f3285e9dcf795bccea6ef
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-05 12:29:43 +0530

    Update main.tf

--- Modules/ec2/main.tf
+++ Modules/ec2/main.tf
@@ -15,6 +15,7 @@ resource "aws_instance" "my-project" {
   count         = var.instance_count
   subnet_id = var.aws_subnet_id
   vpc_security_group_ids = var.aws_security_group
+  availability_zone      = var.availability_zone
 
   tags = {
     Name = "${var.location}${var.app_name}${var.env}${var.service_name}${format("%03d", local.last_used_number + count.index)}"
