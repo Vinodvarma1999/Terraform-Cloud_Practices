9e29f4342e6fe7b153133780d624c118c4253313
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-05 16:28:22 +0530

    Update main.tf

--- Modules/ec2/main.tf
+++ Modules/ec2/main.tf
@@ -1,13 +1,3 @@
-locals {
-  last_used_number = try(tonumber(file("${path.module}/last_used_number.txt")), 1)
-}
-
-# Write the last used number to a local file
-# resource "local_file" "last_used_number_file" {
-#   content  = tostring(local.last_used_number + var.instance_count)
-#   filename = "${path.module}/last_used_number.txt"
-# }
-
 # Creating EC2 Instance
 resource "aws_instance" "my-project" {
   ami = var.aws_ami
@@ -18,6 +8,30 @@ resource "aws_instance" "my-project" {
   availability_zone      = var.availability_zone
 
   tags = {
-    Name = "${var.location}${var.app_name}${var.env}${var.service_name}${format("%03d", local.last_used_number + count.index)}"
+    Name = "${var.location}${var.app_name}${var.env}${var.service_name}${format("%03d", count.index + 1)}"
   }
 } 
+
+# locals {
+#   last_used_number = try(tonumber(file("${path.module}/last_used_number.txt")), 1)
+# }
+
+# Write the last used number to a local file
+# resource "local_file" "last_used_number_file" {
+#   content  = tostring(local.last_used_number + var.instance_count)
+#   filename = "${path.module}/last_used_number.txt"
+# }
+
+# # Creating EC2 Instance
+# resource "aws_instance" "my-project" {
+#   ami = var.aws_ami
+#   instance_type = var.instance_type
+#   count         = var.instance_count
+#   subnet_id = var.aws_subnet_id
+#   vpc_security_group_ids = var.aws_security_group
+#   availability_zone      = var.availability_zone
+
+#   tags = {
+#     Name = "${var.location}${var.app_name}${var.env}${var.service_name}${format("%03d", local.last_used_number + count.index)}"
+#   }
+# } 
