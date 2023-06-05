be16008cbf04df7f16972e66901155decd9eea9b
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-05 11:04:31 +0530

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,12 +1,23 @@
-variable "ec2" {
-  type = map(object({
-    instance_type       = string
-    aws_ami             = string
-    instance_count      = number
-    aws_region          = string
-    aws_access_key      = string
-    aws_secret_key      = string
-    aws_security_group  = list(string)
-    aws_subnet_id       = string
-  }))
+variable "instance_type" {
+}
+
+variable "aws_ami" {
+}
+
+variable "instance_count" {
+}
+
+variable "aws_region"  {
+}
+
+variable "aws_access_key" {
+}
+
+variable "aws_secret_key"  {
+}
+
+variable "aws_security_group" {
+}
+
+variable "aws_subnet_id"  {
 }
