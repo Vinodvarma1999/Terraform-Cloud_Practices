bd88a8072e0f2b9b32c6fd016dac4ecbbaba64d1
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-05 16:40:33 +0530

    Update main.tf

--- Modules/ec2/main.tf
+++ Modules/ec2/main.tf
@@ -1,9 +1,9 @@
 # Creating EC2 Instance
 resource "aws_instance" "my-project" {
-  ami = var.aws_ami
-  instance_type = var.instance_type
-  count         = var.instance_count
-  subnet_id = var.aws_subnet_id
+  ami                    = var.aws_ami
+  instance_type          = var.instance_type
+  count                  = var.instance_count
+  subnet_id              = var.aws_subnet_id
   vpc_security_group_ids = var.aws_security_group
   availability_zone      = var.availability_zone
 
