15a8616694d0b88d435d881f79a115362412932b
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-10 15:17:39 +0530

    Update variables.tf

--- Modules/eks/variables.tf
+++ Modules/eks/variables.tf
@@ -1,34 +1,34 @@
-varibale "cidr_block" {
+variable "cidr_block" {
  type = string
  description = "Represents the CIDR block for the VPC."
 }
 
-varibale "vpc_tags" {
+variable "vpc_tags" {
  type = map(string)
  description = "A map of string values used to tag the VPC resource."
 }
 
-varibale "subnet_cidr_block" {
+variable "subnet_cidr_block" {
  type = string
  description = "Represents the CIDR block for a subnet."
 }
 
-varibale "subnet_availablity_zone" {
+variable "subnet_availablity_zone" {
  type = string
  description = "Represents the availability zone where a subnet will be created."
 }
 
-varibale "public_ip_launch" {
+variable "public_ip_launch" {
  type = bool
  description = "Determines whether instances launched in the subnet will have public IP addresses."
 }
 
-varibale "var.subnet_tags" {
+variable "var.subnet_tags" {
  type = map(string)
  description = "A map of string values used to tag the subnet resource."
 }
 
-varibale "igw_tags" {
+variable "igw_tags" {
  type = map(string)
  description = "A map of string values used to tag the internet gateway resource."
 }
@@ -38,22 +38,22 @@ variable "route_table_tags" {
  description = "A map of string values used to tag the route table resource."
 }
 
-varibale "cluster_iam_role_name" {
+variable "cluster_iam_role_name" {
  type = string
  description = "Represents the name of the IAM role for the EKS cluster."
 }
 
-varibale "policy_arn" {
+variable "policy_arn" {
  type = string
  description = " Represents the ARN (Amazon Resource Name) of a policy."
 }
 
-varibale "eks_name" {
+variable "eks_name" {
  type = string
  description = "Represents the name of the EKS cluster."
 }
 
-varibale "eks_cluster_version" {
+variable "eks_cluster_version" {
  type = string
  description = " Represents the version of the EKS cluster."
 }
@@ -63,12 +63,12 @@ variable "eks_kms_key" {
  description = "The ARN of the Key Management Service (KMS) customer master key (CMK). The CMK must be symmetric, created in the same region as the cluster"
 }
 
-varibale "eks_endpoint_private_access" {
+variable "eks_endpoint_private_access" {
  type = bool
  description = "Determines whether the EKS cluster has private endpoint access."
 }
 
-varibale "eks_endpoint_public_access" {
+variable "eks_endpoint_public_access" {
  type = bool
  description = "Determines whether the EKS cluster has public endpoint access."
 }
@@ -93,42 +93,42 @@ variable "eks_secret" {
  description = "Encryption enabled for secrets."
 }
 
-varibale "eks_cluster_sg_name" {
+variable "eks_cluster_sg_name" {
  type = string
  description = "Represents the name of the security group for EKS cluster."
 }
 
-varibale "eks_cluster_sg_tags" {
+variable "eks_cluster_sg_tags" {
  type = map(string)
  description = "A map of string values used to tag the security group resource for the EKS cluster."
 }
 
-varibale "eks_node_sg_name" {
+variable "eks_node_sg_name" {
  type = string
  description = "Represents the name of the security group for EKS worker nodes."
 }
 
-varibale "eks_node_sg_tags" {
+variable "eks_node_sg_tags" {
  type = map(string)
  description = "A map of string values used to tag the security group resource for EKS worker nodes."
 }
 
-varibale "node_iam_role_name" {
+variable "node_iam_role_name" {
  type = string
  description = "Represents the name of the IAM role for EKS worker nodes."
 }
 
-varibale "node_AmazonEKSWorkerNodePolicy" {
+variable "node_AmazonEKSWorkerNodePolicy" {
  type = string
  description = "Represents the policy associated with EKS worker nodes."
 }
 
-varibale "node_AmazonEKS_CNI_Policy" {
+variable "node_AmazonEKS_CNI_Policy" {
  type = string
  description = "Represents the policy associated with the Amazon EKS CNI (Container Network Interface)."
 }
 
-varibale "node_AmazonEC2ContainerRegistryReadOnly" {
+variable "node_AmazonEC2ContainerRegistryReadOnly" {
  type = string
  description = "Represents the policy associated with read-only access to the Amazon ECR (Elastic Container Registry)."
 }
