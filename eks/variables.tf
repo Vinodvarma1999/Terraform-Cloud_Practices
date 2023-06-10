4fc27ba05802b807671eeaf49dc15208ff08b7fd
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-10 15:42:13 +0530

    Update variables.tf

--- eks/variables.tf
+++ eks/variables.tf
@@ -1,32 +1,34 @@
+# main.tf
+
 variable "eks_cluster" {
   type    = map(any)
   default = {
-    cidr_block                    = string
-    vpc_tags                      = map(string)
-    subnet_cidr_block             = string
-    subnet_availablity_zone       = string
-    public_ip_launch              = bool
-    subnet_tags                   = map(string)
-    igw_tags                      = map(string)
-    route_table_tags              = map(string)
-    cluster_iam_role_name         = string
-    policy_arn                    = string
-    eks_name                      = string
-    eks_cluster_version           = string
-    eks_kms_key                   = string
-    eks_endpoint_private_access   = bool
-    eks_endpoint_public_access    = bool
-    eks_public_access_cidrs       = list(string)
-    eks_service_ipv4_cidr         = string
-    eks_config_map                = bool
-    eks_secret                    = bool
-    eks_cluster_sg_name           = string
-    eks_cluster_sg_tags           = map(string)
-    eks_node_sg_name              = string
-    eks_node_sg_tags              = map(string)
-    node_iam_role_name            = string
-    node_AmazonEKSWorkerNodePolicy = string
-    node_AmazonEKS_CNI_Policy     = string
-    node_AmazonEC2ContainerRegistryReadOnly = string
+    cidr_block                    = var.cidr_block
+    vpc_tags                      = var.vpc_tags
+    subnet_cidr_block             = var.subnet_cidr_block
+    subnet_availablity_zone       = var.subnet_availablity_zone
+    public_ip_launch              = var.public_ip_launch
+    subnet_tags                   = var.subnet_tags
+    igw_tags                      = var.igw_tags
+    route_table_tags              = var.route_table_tags
+    cluster_iam_role_name         = var.cluster_iam_role_name
+    policy_arn                    = var.policy_arn
+    eks_name                      = var.eks_name
+    eks_cluster_version           = var.eks_cluster_version
+    eks_kms_key                   = var.eks_kms_key
+    eks_endpoint_private_access   = var.eks_endpoint_private_access
+    eks_endpoint_public_access    = var.eks_endpoint_public_access
+    eks_public_access_cidrs       = var.eks_public_access_cidrs
+    eks_service_ipv4_cidr         = var.eks_service_ipv4_cidr
+    eks_config_map                = var.eks_config_map
+    eks_secret                    = var.eks_secret
+    eks_cluster_sg_name           = var.eks_cluster_sg_name
+    eks_cluster_sg_tags           = var.eks_cluster_sg_tags
+    eks_node_sg_name              = var.eks_node_sg_name
+    eks_node_sg_tags              = var.eks_node_sg_tags
+    node_iam_role_name            = var.node_iam_role_name
+    node_AmazonEKSWorkerNodePolicy = var.node_AmazonEKSWorkerNodePolicy
+    node_AmazonEKS_CNI_Policy     = var.node_AmazonEKS_CNI_Policy
+    node_AmazonEC2ContainerRegistryReadOnly = var.node_AmazonEC2ContainerRegistryReadOnly
   }
 }
