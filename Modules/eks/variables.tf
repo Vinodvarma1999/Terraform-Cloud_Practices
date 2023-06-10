f8e2cc62fcdc8ae7e65e0acf607870bf58aa2985
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-10 16:07:55 +0530

    Update variables.tf

--- Modules/eks/variables.tf
+++ Modules/eks/variables.tf
@@ -84,12 +84,12 @@ variable "eks_service_ipv4_cidr" {
 } 
 
 variable "eks_config_map" {
- type = bool
+ type = string
  description = "Encryption enabled for config_map."
 }
 
 variable "eks_secret" {
- type = bool
+ type = string
  description = "Encryption enabled for secrets."
 }
 
