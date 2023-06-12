9a074490e421b351880755555ebc3283cd3de5e6
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-12 12:24:47 +0530

    Update main.tf

--- Modules/eks/main.tf
+++ Modules/eks/main.tf
@@ -24,7 +24,7 @@ resource "aws_internet_gateway" "example" {
 resource "aws_route_table" "example" {
  vpc_id        = aws_vpc.example.id
  route {
-   cidr_block  = "0.0.0.0./0"
+   cidr_block  = "0.0.0.0/0"
    gateway_id  = aws_internet_gateway.example.id
  }
  tags          = var.route_table_tags
