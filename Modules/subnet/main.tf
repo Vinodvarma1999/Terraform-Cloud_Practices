7d6cbd19929d48550d6d134d261e25b42fd7fa31
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-06-27 12:07:21 +0530

    Update main.tf

--- Modules/subnet/main.tf
+++ Modules/subnet/main.tf
@@ -3,6 +3,6 @@ resource "aws_subnet" "example" {
   cidr_block              = var.cidr_block
   map_public_ip_on_launch = var.map_public_ip_on_launch
   tags                    = var.tags
-  route_table_id          = var.enable_routing ? var.route_table_id : null
-  network_acl_id          = var.enable_network_acl ? var.network_acl_id : null
+  route_table_id          = var.route_table_id
+  network_acl_id          = var.network_acl_id 
 }
